ee287b55caf980a06d7762e7230a010d0f13c7f2
박동현 <donghyeondev@gmail.com>
2022-11-01 21:55:58 +0900

    initial commit

---
+++ src/Getmax.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Getmax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("3가지의 정수에서 최댓값을 구하라");
+    }
+}
